08cc3274d9758a63eaa0d315807fad4ed308b58b
kbrownierichard <121068968+kbrownierichard@users.noreply.github.com>
2022-12-20 13:14:24 +0000

    add one

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{ pkgs }: {
+	deps = [
+		pkgs.run
+  pkgs.run one
+  pkgs.python38Full
+	];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([
+      # Neded for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+    ] ++ (with pkgs.xlibs; [ libX11 libXext libXinerama libXcursor libXrandr libXi libXxf86vm ]));
+
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+  };
+}
